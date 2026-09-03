61bf17f2a5d8ff10e30d7cc0a72c158ebdacb804
Gaucho Gustavo Furtado <150378625+gustavofurtadopedroso@users.noreply.github.com>
2026-09-03 19:42:36 -0300

    Delete atividade gabirel directory

--- atividade gabirel/script.js
+++
@@ -1,9 +0,0 @@
-const botaoAtualizar = document.querySelector('#atualizarRotina');
-const statusRotina = document.querySelector('#statusRotina');
-
-botaoAtualizar.addEventListener('click', () => {
-    statusRotina.textContent = 'Situação atual: rotina finalizada com sucesso.';
-    statusRotina.classList.add('concluido');
-    botaoAtualizar.textContent = 'Status atualizado';
-    botaoAtualizar.disabled = true;
-});
